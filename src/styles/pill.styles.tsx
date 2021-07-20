09ef76a003f28b391cb1cd1bd23fccd7af752fa3
AlejandroQR23 <alejandro.quijano@comunidad.unam.mx>
2021-07-19 23:28:35 -0500

    created pill shared component

---
+++ src/styles/pill.styles.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Pill = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 4px 8px;
+  margin: 10px 0;
+  background: #eaf8ec;
+  border-radius: 100px;
+
+  font-weight: 800;
+  font-size: 12px;
+  line-height: 12px;
+  text-transform: uppercase;
+  color: #51b960;
+`;
