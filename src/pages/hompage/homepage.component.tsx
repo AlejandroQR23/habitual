62b87c361a0f9957b25773b61cdc331b6994de08
AlejandroQR23 <alejandro.quijano@comunidad.unam.mx>
2021-07-19 23:28:53 -0500

    added hot deals section to the home page

--- src/pages/hompage/homepage.component.tsx
+++ src/pages/hompage/homepage.component.tsx
@@ -1,8 +1,10 @@
 import HomeHero from '../../components/home-hero/home-hero.component';
+import HotDeals from '../../components/hot-deals/hot-deals.component';
 
 const HomePage = () => (
   <div>
     <HomeHero />
+    <HotDeals />
   </div>
 );
 
