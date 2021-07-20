6f1debd7c737d0ad9c911d95278f64eab5714a71
AlejandroQR23 <alejandro.quijano@comunidad.unam.mx>
2021-07-20 18:50:34 -0500

    added color card grid component to the homepage

--- src/pages/hompage/homepage.component.tsx
+++ src/pages/hompage/homepage.component.tsx
@@ -1,3 +1,4 @@
+import ColorCardGrid from '../../components/color-card-grid/color-card-grid.component';
 import HomeHero from '../../components/home-hero/home-hero.component';
 import HotDeals from '../../components/hot-deals/hot-deals.component';
 
@@ -5,6 +6,7 @@ const HomePage = () => (
   <div>
     <HomeHero />
     <HotDeals />
+    <ColorCardGrid />
   </div>
 );
 
