6fd8637ea51e96327bbadde1e6cc301e26f08096
AlejandroQR23 <alejandro.quijano@comunidad.unam.mx>
2021-07-19 23:26:11 -0500

    created hot deals component and its styles

---
+++ src/components/hot-deals/hot-deals.component.tsx
@@ -0,0 +1,11 @@
+import CardLgGrid from '../card-lg-grid/card-lg-grid.component';
+import { HotDealsContainer } from './hot-deals.styles';
+
+const HotDeals = () => (
+  <HotDealsContainer>
+    <h2>Hot Deals</h2>
+    <CardLgGrid />
+  </HotDealsContainer>
+);
+
+export default HotDeals;
---
+++ src/components/hot-deals/hot-deals.styles.tsx
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const HotDealsContainer = styled.div`
+  margin-top: 48px;
+`;
