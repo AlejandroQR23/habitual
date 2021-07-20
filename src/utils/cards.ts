390dfe9c15c1959663e01d97fe3469f6791d40dc
AlejandroQR23 <alejandro.quijano@comunidad.unam.mx>
2021-07-20 18:45:56 -0500

    created mock color cards data

---
+++ src/utils/cards.ts
@@ -0,0 +1,11 @@
+export interface IColorCard {
+  text: string;
+  color: string;
+}
+
+export const colorCards: IColorCard[] = [
+  { text: 'Shopping habits and interests', color: '#FE8668' },
+  { text: "Today's trending items", color: '#30D6B0' },
+  { text: 'Incoming! Flash hot deals', color: '#4269F2' },
+  { text: 'Browse our new categories', color: '#FDBC1F' },
+];
