79fc8f5110ab9beb2cbb90141a1587be91ab1735
AlejandroQR23 <alejandro.quijano@comunidad.unam.mx>
2021-07-19 23:27:37 -0500

    fixed hero styles to adapt to the new global margin and added media queries

--- src/components/home-hero/home-hero.styles.tsx
+++ src/components/home-hero/home-hero.styles.tsx
@@ -1,14 +1,23 @@
 import styled from 'styled-components';
 
 export const HeroContainer = styled.div`
-  margin: 48px 72px 0;
+  margin-top: 48px;
   display: flex;
+  flex-direction: row;
   flex-wrap: wrap;
-  justify-content: space-around;
+  justify-content: space-between;
   align-items: center;
+
+  @media screen and (max-width: 900px) {
+    justify-content: center;
+  }
 `;
 
 export const HeroTextContainer = styled.div`
   max-width: 571px;
-  width: 50%;
+  width: 60%;
+
+  @media screen and (max-width: 900px) {
+    margin-bottom: 32px;
+  }
 `;
