ad470ee53e583100bf6b19e714f6bbc70824b3be
AlejandroQR23 <alejandro.quijano@comunidad.unam.mx>
2021-07-21 18:59:46 -0500

    added a title for category grid

---
+++ src/components/category-card-grid/category-card-grid.styles.tsx
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const CategoryCardGridContainer = styled.div`
+  margin-top: 48px;
+`;
--- src/components/category-card-grid/category-grid.component.tsx
+++ src/components/category-card-grid/category-grid.component.tsx
@@ -1,14 +1,18 @@
 import { categoryCards } from '../../utils/cards';
 import { CardGridContainer } from '../../styles/card-grid.styles';
 import CategoryCard from '../category-card/category-card.component';
+import { CategoryCardGridContainer } from './category-card-grid.styles';
 
 const CategoryCardGrid = () => {
   return (
-    <CardGridContainer>
-      {categoryCards.map((item, index) => (
-        <CategoryCard key={index} text={item.text} color={item.color} img={item.img} />
-      ))}
-    </CardGridContainer>
+    <CategoryCardGridContainer>
+      <h2>Categories</h2>
+      <CardGridContainer>
+        {categoryCards.map((item, index) => (
+          <CategoryCard key={index} text={item.text} color={item.color} img={item.img} />
+        ))}
+      </CardGridContainer>
+    </CategoryCardGridContainer>
   );
 };
 
