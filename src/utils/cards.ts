be6e67e6e41bf41fa356228137a1a4fdae363cee
AlejandroQR23 <alejandro.quijano@comunidad.unam.mx>
2021-07-21 18:48:46 -0500

    added category card mockup

--- src/utils/cards.ts
+++ src/utils/cards.ts
@@ -1,6 +1,7 @@
 export interface IColorCard {
   text: string;
   color: string;
+  img?: string;
 }
 
 export const colorCards: IColorCard[] = [
@@ -8,4 +9,12 @@ export const colorCards: IColorCard[] = [
   { text: "Today's trending items", color: '#30D6B0' },
   { text: 'Incoming! Flash hot deals', color: '#4269F2' },
   { text: 'Browse our new categories', color: '#FDBC1F' },
+];
+
+export const categoryCards: IColorCard[] = [
+  { text: 'Electronics', color: '#FE8668', img: '../../assets/bose-headset.png' },
+  { text: 'Gaming', color: '#30D6B0', img: '../../assets/bose-headset.png' },
+  { text: 'Fashion', color: '#4269F2', img: '../../assets/bose-headset.png' },
+  { text: 'Education', color: '#FDBC1F', img: '../../assets/bose-headset.png' },
+  { text: 'Music', color: '#FE8668', img: '../../assets/bose-headset.png' },
 ];
