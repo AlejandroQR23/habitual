ec129a049b8f3f503efd51c13cf1e68c9819e96e
AlejandroQR23 <alejandro.quijano@comunidad.unam.mx>
2021-07-29 20:10:35 -0500

    added routing for the category page

--- src/pages/shop/shop.component.tsx
+++ src/pages/shop/shop.component.tsx
@@ -1,14 +1,27 @@
+import { Route, RouteComponentProps } from 'react-router-dom';
 import CategoryCardGrid from '../../components/category-card-grid/category-grid.component';
 import FlashDeals from '../../components/flash-deals/flash-deals.component';
 import ShopHero from '../../components/shop-hero/shop-hero.component';
+import CategoryPage from '../category/category.component';
 import { ShopContainer } from './shop.styles';
 
-const Shop = () => (
-  <ShopContainer>
-    <ShopHero />
-    <CategoryCardGrid />
-    <FlashDeals />
-  </ShopContainer>
-);
+const Shop = ({ match }: RouteComponentProps) => {
+  return (
+    <div>
+      <Route
+        exact
+        path={`${match.path}`}
+        render={() => (
+          <ShopContainer>
+            <ShopHero />
+            <CategoryCardGrid />
+            <FlashDeals />
+          </ShopContainer>
+        )}
+      />
+      <Route path={`${match.path}/:collectionId`} component={CategoryPage} />
+    </div>
+  );
+};
 
 export default Shop;
