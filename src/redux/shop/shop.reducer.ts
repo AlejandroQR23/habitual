d0fed0e7c59639b568e76a05626f7fdbeec18f50
AlejandroQR23 <alejandro.quijano@comunidad.unam.mx>
2021-07-29 20:11:10 -0500

    created shop selectors for category (by name) and categories

--- src/redux/shop/shop.reducer.ts
+++ src/redux/shop/shop.reducer.ts
@@ -1,7 +1,8 @@
 import { SHOP_DATA } from './shop-data';
 import { createSlice } from '@reduxjs/toolkit';
 import { RootState } from '../store';
-// import { RootState } from '../store';
+import { useAppSelector } from '../hooks';
+// import { ICategory } from '../directory/directory.reducer';
 
 export const INITIAL_SHOP = {
   categories: SHOP_DATA,
@@ -15,8 +16,14 @@ export const shopSlice = createSlice({
 
 // * Selectors
 export const selectShopCategories = (state: RootState) => state.shop.categories;
+
+export const selectCategory = (categoryId: string) => {
+  const categories = useAppSelector(selectShopCategories);
+  return categories[categoryId];
+};
+
 export const selectDiscountShopItems = (state: RootState) =>
-  state.shop.categories[0].products.filter((item) => item.discount > 0);
+  state.shop.categories.misc.products.filter((item) => item.discount > 0);
 
 // * Reducer
 export default shopSlice.reducer;
