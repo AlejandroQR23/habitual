753cd975bc06a16ed7f8d10e7c886c7f56ee5302
AlejandroQR23 <alejandro.quijano@comunidad.unam.mx>
2021-07-31 00:42:02 -0500

    added a background for the checkout page

--- src/pages/checkout/checkout.styles.tsx
+++ src/pages/checkout/checkout.styles.tsx
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 export const CheckoutContainer = styled.div`
   text-align: center;
   justify-content: center;
+  background-image: url('https://firebasestorage.googleapis.com/v0/b/habitual-qr23.appspot.com/o/cart-background.svg?alt=media&token=3759ae5d-3a0d-4306-85e7-e385ea513e4f');
+  background-position: center;
 `;
 
 export const CheckoutItemsContainer = styled.div`
