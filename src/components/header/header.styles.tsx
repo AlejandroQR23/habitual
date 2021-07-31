f02ebf0af4963eb05faaea13f110b0cf77c37071
AlejandroQR23 <alejandro.quijano@comunidad.unam.mx>
2021-07-31 00:40:37 -0500

    improved mobile view for the header

--- src/components/header/header.styles.tsx
+++ src/components/header/header.styles.tsx
@@ -12,15 +12,34 @@ export const HeaderContainer = styled.header`
   width: 100%;
   z-index: 99;
   top: 0;
+
+  @media screen and (max-width: 580px) {
+    .logo {
+      width: 100px;
+    }
+  }
 `;
 
 export const OptionsContainer = styled.div`
   width: 50%;
   display: flex;
   justify-content: flex-end;
+
+  @media screen and (max-width: 580px) {
+    justify-content: center;
+    svg {
+      width: 16px;
+    }
+  }
 `;
 
 export const OptionLink = styled(Link)`
   cursor: pointer;
   padding: 0 32px;
+
+  @media screen and (max-width: 580px) {
+    font-size: 12px;
+    line-height: 16px;
+    padding: 0 16px;
+  }
 `;
