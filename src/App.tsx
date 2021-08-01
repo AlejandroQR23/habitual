40373e8643996276b635de5e9dc083cb4985f8a4
AlejandroQR23 <alejandro.quijano@comunidad.unam.mx>
2021-07-31 20:10:09 -0500

    added under construcion page to the app routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Switch } from 'react-router-dom';
 import Footer from './components/footer/footer.component';
 import Header from './components/header/header.component';
 import Checkout from './pages/checkout/checkout.component';
+import Construction from './pages/construction/construction.component';
 import HomePage from './pages/hompage/homepage.component';
 import Shop from './pages/shop/shop.component';
 import { GlobalStyle } from './styles/global.styles';
@@ -17,6 +18,8 @@ function App() {
           <Route exact path="/" component={HomePage} />
           <Route exact path="/checkout" component={Checkout} />
           <Route path="/shop" component={Shop} />
+          <Route path="/about" component={Construction} />
+          <Route path="/signin" component={Construction} />
         </PageContent>
       </Switch>
       <Footer />
