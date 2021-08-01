a7b36675d0b96496b0b8d4259534c9e619c03cae
AlejandroQR23 <alejandro.quijano@comunidad.unam.mx>
2021-07-31 20:08:23 -0500

    added entrance animation for checkout and category pages

--- src/pages/checkout/checkout.styles.tsx
+++ src/pages/checkout/checkout.styles.tsx
@@ -5,6 +5,8 @@ export const CheckoutContainer = styled.div`
   justify-content: center;
   background-image: url('https://firebasestorage.googleapis.com/v0/b/habitual-qr23.appspot.com/o/cart-background.svg?alt=media&token=3759ae5d-3a0d-4306-85e7-e385ea513e4f');
   background-position: center;
+
+  animation: slide-in-fwd-top 0.4s cubic-bezier(0.25, 0.46, 0.45, 0.94) both;
 `;
 
 export const CheckoutItemsContainer = styled.div`
